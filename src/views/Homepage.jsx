d9e17cf66e058697dd93b3f538f1c48c41f7e939
faseian <nab4nkert@outlook.com>
2025-01-30 19:27:15 -0500

    Start

---
+++ src/views/Homepage.jsx
@@ -0,0 +1,10 @@
+import { Container, Stack, Typography } from '@mui/material'
+export default function Homepage() {
+  return (
+        <Container>
+            <Stack>
+                <Typography>Hello</Typography>
+            </Stack>
+        </Container>
+    )
+}
