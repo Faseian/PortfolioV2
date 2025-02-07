dd917ef6341e7eeb72fcdde6aebcd495e8e84399
faseian <nab4nkert@outlook.com>
2025-02-06 20:18:53 -0500

    Changed RouterProvider to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { createRoot } from 'react-dom/client'
 import theme from './assets/theme.js'
 import Homepage from './views/Homepage.jsx'
 import { Box, ThemeProvider } from '@mui/material'
-import { RouterProvider, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import CssBaseline from '@mui/material/CssBaseline'
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider>
+  <BrowserRouter>
     <ThemeProvider theme={theme}>
       <CssBaseline/>
       <Box
@@ -14,11 +14,11 @@ createRoot(document.getElementById('root')).render(
         flexDirection='column'
         justifyContent='center'
       >
-        <Routes >
+        <Routes>
           <Route index path='/' element={<Homepage/>}>
           </Route>
         </Routes>
       </Box>
     </ThemeProvider>
-  </RouterProvider>,
+  </BrowserRouter>,
 )
